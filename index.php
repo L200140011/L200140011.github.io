48bb2000882fbb9d9eafc01dfb4b6e941899aac6
L200140011 <naisharahmai@gmail.com>
2017-07-06 23:18:39 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Home | L200140011</title>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<!-- Memanggil pustaka Google Map -->
+	<SCRIPT src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAXAJO4mUDJ_yfJKtISReCE79mjkYkmJAQ"></SCRIPT>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	<link rel="stylesheet" type="text/css" href="css.css">
+
+<style type="text/css">
+	body{
+		margin-top:100px;
+		font-family: Calibri;
+	}
+	.well{
+			text-align: center;
+			font-size: 25px;
+		}
+	.glyphicon { margin-right:10px; }
+	.panel-body { padding:0px; }
+	.panel-body table tr td { padding-left: 15px }
+	.panel-body .table {margin-bottom: 0px; }
+	a:link {text-decoration: none}
+	a:visited {text-decoration: none}
+	a:active {text-decoration: none}
+	h1{font-family: Calibri;}
+	 #pages {
+	   padding:8px 16px;
+	   border:1px solid #ccc;
+	   color:#408c99;
+	   font-weight:bold;
+	  }
+</style>
+	
+
+
+<style>
+	body{margin-top:100px;}
+	.glyphicon { margin-right:10px; }
+	.panel-body { padding:0px; }
+	.panel-body table tr td { padding-left: 15px }
+	.panel-body .table {margin-bottom: 0px; }
+	a:link {text-decoration: none}
+	a:visited {text-decoration: none}
+	a:active {text-decoration: none}
+	h1{font-family: Calibri;}
+	 #pages {
+	   padding:8px 16px;
+	   border:1px solid #ccc;
+	   color:#408c99;
+	   font-weight:bold;
+	  }
+	  
+.myButton {
+	-moz-box-shadow: 0px 10px 14px -7px #276873;
+	-webkit-box-shadow: 0px 10px 14px -7px #276873;
+	box-shadow: 0px 10px 14px -7px #276873;
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #599bb3), color-stop(1, #408c99));
+	background:-moz-linear-gradient(top, #599bb3 5%, #408c99 100%);
+	background:-webkit-linear-gradient(top, #599bb3 5%, #408c99 100%);
+	background:-o-linear-gradient(top, #599bb3 5%, #408c99 100%);
+	background:-ms-linear-gradient(top, #599bb3 5%, #408c99 100%);
+	background:linear-gradient(to bottom, #599bb3 5%, #408c99 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#599bb3', endColorstr='#408c99',GradientType=0);
+	background-color:#599bb3;
+	-moz-border-radius:8px;
+	-webkit-border-radius:8px;
+	border-radius:8px;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:13px 32px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #3d768a;
+}
+.myButton:hover {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #408c99), color-stop(1, #599bb3));
+	background:-moz-linear-gradient(top, #408c99 5%, #599bb3 100%);
+	background:-webkit-linear-gradient(top, #408c99 5%, #599bb3 100%);
+	background:-o-linear-gradient(top, #408c99 5%, #599bb3 100%);
+	background:-ms-linear-gradient(top, #408c99 5%, #599bb3 100%);
+	background:linear-gradient(to bottom, #408c99 5%, #599bb3 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#408c99', endColorstr='#599bb3',GradientType=0);
+	background-color:#408c99;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
+
+
+</style>
+
+</head>
+<body>
+	<div class="row">
+		<div class="container">
+    	<div class="col-md-14">
+		<nav class = "navbar navbar-default navbar-fixed-top" role = "navigation">
+
+		<a href = "index.php"><center><h3><label>Sistem Informasi Geografi 2017</label></h3></center></a>
+	</div>
+	<div class="row">
+		<div class="container">
+			<div class="col-md-3">
+				<div class="panel-group">
+				<a href="index.php" class="myButton" width='30'>
+								<h4 class="panel-title">
+	                            	HOME
+	                        	</h4>
+							</a><br><br>
+	                        <a href="https://github.com/L200140011/Sistem-Informasi-Geografi-2017/" class="myButton">
+								<h4 class="panel-title">
+	                            	Tugas 1 
+	                        	</h4>
+							</a><br><br>
+							<a href="tugas2.php" class="myButton">
+								<h4 class="panel-title">
+	                            	Tugas 2 
+	                        	</h4>
+							</a><br><br><a href="tugas3.php" class="myButton">
+								<h4 class="panel-title">
+	                            	Tugas 3 
+	                        	</h4>
+							</a><br><br><a href="tugas4.php" class="myButton">
+								<h4 class="panel-title">
+	                            	Tugas 4 
+	                        	</h4>
+							</a><br><br><a href="tugas5.php" class="myButton">
+								<h4 class="panel-title">
+	                            	Tugas 5 
+	                        	</h4>
+							</a><br><br><a href="https://github.com/L200140011/Sistem-Informasi-Geografi-2017/" class="myButton">
+								<h4 class="panel-title">
+	                            	myGithub
+	                        	</h4>
+							</a>
+				</div>				
+				</div>					
+		<div class="col-md-9">
+    		<div class="panel-body page-contents">
+
+    		<div id="tugas1" class="content-p collapse in">
+    		<div class="well">
+				<b>TUGAS<br/>SISTEM INFORMASI GEOGRAFI 2017<br/><br>
+
+				NAISHA RAHMA INDRASWARI<br/>
+				L200140011<br/>KELAS A<br/><br>
+
+				PROGRAM STUDI INFORMATIKA<br/>
+				FAKULTAS KOMUNIKASI DAN INFORMATIKA<br/>
+				UNIVERSITAS MUHAMMADIYAH SURAKARTA<br/>2017</b>
+    		</div>
+    		</div>
+    </div>
+    </div>
+
+
+			
+</body>
+</html>
---
+++ tugas5.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Tugas 5 | L200140011</title>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<!-- Memanggil pustaka Google Map -->
+	<SCRIPT src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAXAJO4mUDJ_yfJKtISReCE79mjkYkmJAQ"></SCRIPT>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!-- Memanggil pustaka Google Map -->
+	<script type="Text/Javascript" src="geolocation.js"></script>
+ 
+	<!-- CSS Untuk mengatur ukuran Peta -->
+	<style type="text/css">
+		body{
+			font-family: Calibri;
+		}
+		#mapDiv { 
+			width: 785px; 
+			height: 350px;
+		}
+	</style>
+ 
+	
+	<link rel="stylesheet" type="text/css" href="css.css">
+
+<style type="text/css">
+	body{
+		margin-top:100px;
+		font-family: Calibri;
+	}
+	.well{
+			text-align: center;
+			font-size: 25px;
+		}
+	.glyphicon { margin-right:10px; }
+	.panel-body { padding:0px; }
+	.panel-body table tr td { padding-left: 15px }
+	.panel-body .table {margin-bottom: 0px; }
+	a:link {text-decoration: none}
+	a:visited {text-decoration: none}
+	a:active {text-decoration: none}
+	h1{font-family: Calibri;}
+	 #pages {
+	   padding:8px 16px;
+	   border:1px solid #ccc;
+	   color:#408c99;
+	   font-weight:bold;
+	  }
+</style>
+	
+
+
+<style>
+	body{margin-top:100px;}
+	.glyphicon { margin-right:10px; }
+	.panel-body { padding:0px; }
+	.panel-body table tr td { padding-left: 15px }
+	.panel-body .table {margin-bottom: 0px; }
+	a:link {text-decoration: none}
+	a:visited {text-decoration: none}
+	a:active {text-decoration: none}
+	h1{font-family: Calibri;}
+	 #pages {
+	   padding:8px 16px;
+	   border:1px solid #ccc;
+	   color:#408c99;
+	   font-weight:bold;
+	  }
+	  
+.myButton {
+	-moz-box-shadow: 0px 10px 14px -7px #276873;
+	-webkit-box-shadow: 0px 10px 14px -7px #276873;
+	box-shadow: 0px 10px 14px -7px #276873;
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #599bb3), color-stop(1, #408c99));
+	background:-moz-linear-gradient(top, #599bb3 5%, #408c99 100%);
+	background:-webkit-linear-gradient(top, #599bb3 5%, #408c99 100%);
+	background:-o-linear-gradient(top, #599bb3 5%, #408c99 100%);
+	background:-ms-linear-gradient(top, #599bb3 5%, #408c99 100%);
+	background:linear-gradient(to bottom, #599bb3 5%, #408c99 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#599bb3', endColorstr='#408c99',GradientType=0);
+	background-color:#599bb3;
+	-moz-border-radius:8px;
+	-webkit-border-radius:8px;
+	border-radius:8px;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:13px 32px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #3d768a;
+}
+.myButton:hover {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #408c99), color-stop(1, #599bb3));
+	background:-moz-linear-gradient(top, #408c99 5%, #599bb3 100%);
+	background:-webkit-linear-gradient(top, #408c99 5%, #599bb3 100%);
+	background:-o-linear-gradient(top, #408c99 5%, #599bb3 100%);
+	background:-ms-linear-gradient(top, #408c99 5%, #599bb3 100%);
+	background:linear-gradient(to bottom, #408c99 5%, #599bb3 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#408c99', endColorstr='#599bb3',GradientType=0);
+	background-color:#408c99;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
+
+
+</style>
+<!-- membuat script untuk menampilkan peta -->
+</head>
+<body>
+	<div class="row">
+		<div class="container">
+    	<div class="col-md-14">
+		<nav class = "navbar navbar-default navbar-fixed-top" role = "navigation">
+
+		<a href = "#"><center><h3><label>TUGAS 5  Gabungan Chapter 7 <br> Naisha Rahma Indraswari | L200140011 | Kelas A</label></h3></center></a>
+	</div>
+	<br>
+	<div class="row">
+		<div class="container">
+			<div class="col-md-3">
+				<div class="panel-group">
+				<a href="index.php" class="myButton" width='30'>
+								<h4 class="panel-title">
+	                            	HOME
+	                        	</h4>
+							</a><br><br>
+	                        <a href="https://github.com/L200140011/Sistem-Informasi-Geografi-2017/" class="myButton">
+								<h4 class="panel-title">
+	                            	Tugas 1 
+	                        	</h4>
+							</a><br><br>
+							<a href="tugas2.php" class="myButton">
+								<h4 class="panel-title">
+	                            	Tugas 2 
+	                        	</h4>
+							</a><br><br><a href="tugas3.php" class="myButton">
+								<h4 class="panel-title">
+	                            	Tugas 3 
+	                        	</h4>
+							</a><br><br><a href="tugas4.php" class="myButton">
+								<h4 class="panel-title">
+	                            	Tugas 4 
+	                        	</h4>
+							</a><br><br><a href="tugas5.php" class="myButton">
+								<h4 class="panel-title">
+	                            	Tugas 5 
+	                        	</h4>
+							</a><br><br><a href="https://github.com/L200140011/Sistem-Informasi-Geografi-2017/" class="myButton">
+								<h4 class="panel-title">
+	                            	myGithub
+	                        	</h4>
+							</a>
+				</div>				
+				</div>					
+
+		<div class="col-md-9">
+    		<div class="panel-body page-contents">
+    		<div id="tugas2" class="content-p collapse in">
+    		<div class="well">
+			<a href = "#"><center><h2><label>Gabungan Chapter 7</label></h2></center></a>
+			<a href="tugas5_alamat.html" class="myButton" role="button">Alamat</a>
+			<a href="tugas5_koordinat.html" class="myButton" role="button">Koordinat</a>
+			<a href="tugas5_elevasi.html" class="myButton" role="button">Ketinggian</a>
+			<a href="tugas5_jarak.html" class="myButton" role="button">Jarak</a>
+			<a href="tugas5_rute.html" class="myButton" role="button">Rute</a>
+			<a href="tugas5_street_view.html" class="myButton" role="button">Street View</a>
+
+    </div>
+    </div>
+    </div>
+    </div>
+
+
+			
+</body>
+</html>
